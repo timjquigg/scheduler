854cded767a35b8237595e21bbca82d6f1b8e4cc
Tim Quigg <timjquigg@gmail.com>
2022-12-02 15:56:20 -0700

    Remove unused test data from Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,46 +6,6 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay, getInterviewersForDay, getInterview } from "helpers/selectors";
 
-
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
-
 export default function Application(props) {
 
   const [state, setState] = useState({
@@ -72,6 +32,7 @@ export default function Application(props) {
   },[]);
   
   const interviewers = getInterviewersForDay(state, state.day);
+  
   const appointmentList = dailyAppointments.map((app) => {
     const interview = getInterview(state, app.interview);
     return (
