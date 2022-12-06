e0e7d69609c88a359d677d02968270d6777873eb
Tim Quigg <timjquigg@gmail.com>
2022-12-05 22:34:23 -0700

    Update to re-introduce update of number of available spots

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,28 +1,8 @@
-/* import {useState, useEffect} from "react";
+import {useReducer, useEffect} from "react";
 import axios from 'axios';
 
 export default function useApplicationData(){
   
-  const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-  
-  const setDay = day => setState({...state, day});
-  
-  useEffect(()=>{
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then((all)=> {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers:all[2].data}))
-    })
-    
-  },[]);
-
   const getSpots = (state, newAppointements) => {
     const dayIndex = state.days.findIndex(day => day.name === state.day);
     const currentDay = state.days[dayIndex];
@@ -33,62 +13,7 @@ export default function useApplicationData(){
     const spots = listOfFreeAppointments.length;
     return [dayIndex, spots];
   }
-  
-  const bookInterview = (id, interview) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    const [today, spots] = getSpots(state, appointments);
-    const day = {...state.days[today], spots:spots};
-    const days = [...state.days];
-    days.splice(today, 1, day);
-    return axios.put(`/api/appointments/${id}`, {interview})
-    .then(()=> {
-      setState(prev => ({...prev, appointments, days}))
-    })
-  }
-  
-  const cancelInterview = (id) => {
-    const interview = null;
-    const appointment = {
-      ...state.appointments[id],
-      interview
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    const [today, spots] = getSpots(state, appointments);
-    const day = {...state.days[today], spots:spots};
-    const days = [...state.days]
-    days.splice(today,1,day);
-    return axios.delete(`/api/appointments/${id}`)
-      .then(() => {
-        setState(prev => ({...prev, appointments, days}))
-    })
-  }
 
-
-
-  return {
-    state,
-    setDay,
-    bookInterview,
-    cancelInterview
-  }
-}
- */
-
-import {useReducer, useEffect} from "react";
-import axios from 'axios';
-
-export default function useApplicationData(){
-  
   const SET_DAY = "SET_DAY";
   const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
   const SET_INTERVIEW = "SET_INTERVIEW";
@@ -103,13 +28,17 @@ export default function useApplicationData(){
       case SET_INTERVIEW: {
         const appointment = {
           ...state.appointments[action.id],
-          interview: { ...action.interview }
+          interview: action.interview
         };
         const appointments = {
           ...state.appointments,
           [action.id]: appointment
         };
-        return {...state, appointments}
+        const [today, spots] = getSpots(state, appointments);
+        const day = {...state.days[today], spots:spots};
+        const days = [...state.days];
+        days.splice(today, 1, day);
+        return {...state, appointments, days}
       }
       default:
         throw new Error(
@@ -140,7 +69,7 @@ export default function useApplicationData(){
     })
     
   },[]);
-
+  
   const bookInterview = (id, interview) => {
     return axios.put(`/api/appointments/${id}`, {interview})
     .then(()=> {
@@ -155,7 +84,7 @@ export default function useApplicationData(){
     return dispatch({type: SET_INTERVIEW, id, interview: null})
     })
   };
-  
+
 
   return {
     state,
