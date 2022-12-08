0b30fe7a0c2e4ca4f2cd476892e89cac9daa8c11
Tim Quigg <timjquigg@gmail.com>
2022-12-07 20:30:40 -0700

    Return SET_DAY case to reducer to fix failure produced by last commit

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -19,8 +19,8 @@ export const SET_INTERVIEW = "SET_INTERVIEW";
 export const reducer = (state, action) => {
   
   switch (action.type) {
-    // case SET_DAY:
-    //   return {...state, day: action.day};
+    case SET_DAY:
+      return {...state, day: action.day};
     case SET_APPLICATION_DATA:
       return {...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers};
     case SET_INTERVIEW: {
